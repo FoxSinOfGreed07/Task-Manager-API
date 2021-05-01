3f547a7c222153543baa5bc489f90ee4ca997df7
FoxSinOfGreed07 <57114107+FoxSinOfGreed07@users.noreply.github.com>
2021-05-01 08:52:47 +0530

    removed pass from code

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
     service : 'gmail',
     auth : {
         user : 'priyanshuv2001@gmail.com',
-        pass : 'crimsondawn'
+        pass : '******'
     }
 })
 
